931ba0901e7c6bffd15650d0b49277e0bcacebfb
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-02 08:55:16 +0900

    백준 영수증

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -6,29 +6,15 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int n3 = sc.nextInt();
-
-        if(n1 == n2 && n2 == n3) {
-            System.out.println(10000 + n1 * 1000);
-        } else if (n1 == n2) {
-            System.out.println(1000 + n1 * 100);
-        } else if (n1 == n3) {
-            System.out.println(1000 + n1 * 100);
-        } else if (n2 == n3) {
-            System.out.println(1000 + n2 * 100);
-        } else {
-            if (n1 > n2 && n1 > n3) {
-                System.out.println(n1 * 100);
-            }
-            if (n2 > n1 && n2 > n3) {
-                System.out.println(n2 * 100);
-            }
-            if (n3 > n1 && n3 > n2) {
-                System.out.println(n3 * 100);
-            }
+        int totalPrice = sc.nextInt();
+        int num = sc.nextInt();
+        int sumPrice = 0;
+        for (int i = 1; i <= num; i++) {
+            int price = sc.nextInt();
+            int quantity = sc.nextInt();
+            sumPrice += price * quantity;
         }
-        sc.close();
+        if (totalPrice == sumPrice) System.out.println("Yes");
+        else if (totalPrice != sumPrice) System.out.println("No");
     }
 }
