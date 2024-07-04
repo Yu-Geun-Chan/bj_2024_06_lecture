573b39332689a2f9fc795ca4288e2c876778d1a7
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-04 15:34:44 +0900

    최댓값, 위치찾기

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,6 +1,6 @@
 package org.koreait;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 class Main {
@@ -9,14 +9,21 @@ class Main {
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
-        int[] arr = new int[num];
+        int[] arr = new int[9];
 
-        for (int i = 0; i < num; i++) {
+        for (int i = 0; i < 9; i++) {
             int a = sc.nextInt();
             arr[i] = a;
         }
-        Arrays.sort(arr);
-        System.out.print(arr[0] + " ");
-        System.out.print(arr[num - 1]);
+        int max = arr[0];
+        int num1 = 1;
+        for (int i = 0; i < 9; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                num1 = i + 1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(num1);
     }
 }
