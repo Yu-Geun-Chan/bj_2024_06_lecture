196f8b27bf640250088e1c29e3ed32c18eaaac4c
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-05 15:29:02 +0900

    백준 나머지

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,29 +1,29 @@
 package org.koreait;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt();
-        int[] arr = new int[9];
+        int[] arr = new int[10];
 
-        for (int i = 0; i < 9; i++) {
-            int a = sc.nextInt();
-            arr[i] = a;
+        for (int i = 0; i < 10; i++) {
+            int num = sc.nextInt();
+            arr[i] = num % 42;
         }
-        int max = arr[0];
-        int num1 = 1;
-        for (int i = 0; i < 9; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                num1 = i + 1;
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                count++;
             }
         }
-        System.out.println(max);
-        System.out.println(num1);
+        if (count >= 9) {
+            count = 9;
+        }
+        System.out.println(10 - count);
     }
 }
