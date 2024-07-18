4761e7a551fce90031412e2b5bb95f4411b13a66
Yu-Geun-Chan <geunchna9055@gmail.com>
2024-07-18 09:17:23 +0900

    문자와 문자열

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,29 +1,17 @@
 package org.koreait;
 
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[10];
+        String cmd = sc.nextLine();
+        int n = sc.nextInt();
 
-        for (int i = 0; i < 10; i++) {
-            int num = sc.nextInt();
-            arr[i] = num % 42;
-        }
-        Arrays.sort(arr);
-        int count = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == arr[i + 1]) {
-                count++;
-            }
-        }
-        if (count >= 9) {
-            count = 9;
-        }
-        System.out.println(10 - count);
+        String[] cmdBits = cmd.split("");
+
+        System.out.println(cmdBits[n - 1]);
+
     }
 }
