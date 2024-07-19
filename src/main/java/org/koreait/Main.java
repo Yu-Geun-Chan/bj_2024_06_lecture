0a6521939ec2db53a431e86a14f4fe0897cb2768
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-19 11:09:22 +0900

    알파벳 찾기

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,25 +1,39 @@
 package org.koreait;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
         String cmd = sc.next();
 
-        String[] cmdBits = cmd.split("");
-
-        int[] nums = new int[n];
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(cmdBits[i]);
-            sum += nums[i];
-        }
-
-        System.out.println(sum);
-
+        System.out.print(cmd.indexOf("a")  + " ");
+        System.out.print(cmd.indexOf("b")  + " ");
+        System.out.print(cmd.indexOf("c")  + " ");
+        System.out.print(cmd.indexOf("d")  + " ");
+        System.out.print(cmd.indexOf("e")  + " ");
+        System.out.print(cmd.indexOf("f")  + " ");
+        System.out.print(cmd.indexOf("g")  + " ");
+        System.out.print(cmd.indexOf("h")  + " ");
+        System.out.print(cmd.indexOf("i")  + " ");
+        System.out.print(cmd.indexOf("j")  + " ");
+        System.out.print(cmd.indexOf("k")  + " ");
+        System.out.print(cmd.indexOf("l")  + " ");
+        System.out.print(cmd.indexOf("m")  + " ");
+        System.out.print(cmd.indexOf("n")  + " ");
+        System.out.print(cmd.indexOf("o")  + " ");
+        System.out.print(cmd.indexOf("p")  + " ");
+        System.out.print(cmd.indexOf("q")  + " ");
+        System.out.print(cmd.indexOf("r")  + " ");
+        System.out.print(cmd.indexOf("s")  + " ");
+        System.out.print(cmd.indexOf("t")  + " ");
+        System.out.print(cmd.indexOf("u")  + " ");
+        System.out.print(cmd.indexOf("v")  + " ");
+        System.out.print(cmd.indexOf("w")  + " ");
+        System.out.print(cmd.indexOf("x")  + " ");
+        System.out.print(cmd.indexOf("y")  + " ");
+        System.out.print(cmd.indexOf("z"));
     }
 }
