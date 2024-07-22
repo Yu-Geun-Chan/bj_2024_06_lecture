0a3ea46180edb72c0a7f85335a78490aabba77a1
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-22 09:14:07 +0900

    1차원배열 바구니 뒤집기

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -2,14 +2,33 @@ package org.koreait;
 
 import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String cmd = sc.nextLine().trim();
-        String[] cmdBits = cmd.split(" ");
-        if(cmdBits[0].equals("")) {
-            System.out.println("0");
-        } else System.out.println(cmdBits.length);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a; // i번째 바구니
+        int b; // j번째 바구니
+        int[] basket = new int[n]; // 바구니 배열
+        int[] reverse = new int[n]; // 뒤집는거 넣는 배열
+
+        for (int i = 0; i < n; i++) {
+            basket[i] = i+1; // 배열은 0부터 숫자는 1부터 시작이므로 +1
+        }
+
+        for (int i = 0; i < m; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            for (int j = a; j <= b; j++) {
+                reverse[a + b - j - 1] = basket[j - 1]; // 숫자는 1부터 시작 배열은 0부터 시작, 따라서 -1 해야됨
+            }
+            for (int j = a; j <= b; j++) {
+                basket[j - 1] = reverse[j - 1];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(basket[i] + " ");
+        }
     }
 }
