80891c7fe2cd5d0fa09ca41c2a4764f7a85096a2
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-23 13:42:53 +0900

    1치원 배열, 평균

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,26 +1,30 @@
 package org.koreait;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = sc.nextInt();
+        int[] scores = new int[n];
 
-        String input = "";
-
-        while((input = br.readLine()) != null) {
-            bw.write(input+"\n");
+        for (int i = 0; i < n; i++) {
+            int score = sc.nextInt();
+            scores[i] = score;
         }
+        Arrays.sort(scores);
 
-        br.close();
-        bw.flush();
-        bw.close();
+        double sum = 0;
+        double average = 0;
+        for (int i = 0; i < n; i++) {
+            sum += ((double) scores[i] / scores[n - 1]) * 100;
+        }
+        average = sum / n;
+        System.out.println(average);
 
+        sc.close();
     }
 }
 
