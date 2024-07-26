987da657f487d2dfc60f34451635d2a0925d959e
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-26 09:06:03 +0900

    심화1, 별찍기7

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int num = sc.nextInt();//
 
         for (int i = 1; i <= num; i++) {
             for (int j = i; j < num; j++) {
